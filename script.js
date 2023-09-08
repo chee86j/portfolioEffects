1a8d8b4c303c88d27c09a4e845dbd1cfe67415d0
chee86j <jeffreychee86@gmail.com>
2023-09-08 18:36:23 -0400

    fixed responsive design added tilt effect

--- script.js
+++ script.js
@@ -4,3 +4,59 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// Code for the Tilt Effect
+class VanillaTilt {
+  constructor(element) {
+    this.element = element;
+    this.settings = {
+      scale: 1,
+      perspective: 1000,
+      max: 15,
+    };
+    this.init();
+  }
+
+  init() {
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.element.addEventListener("mouseenter", this.onMouseEnter.bind(this));
+    this.element.addEventListener("mousemove", this.onMouseMove.bind(this));
+    this.element.addEventListener("mouseleave", this.onMouseLeave.bind(this));
+  }
+
+  onMouseEnter(event) {
+    this.update(event);
+  }
+
+  onMouseMove(event) {
+    this.update(event);
+  }
+
+  onMouseLeave() {
+    this.reset();
+  }
+
+  update(event) {
+    const rect = this.element.getBoundingClientRect();
+    const x = (event.clientX - rect.left) / rect.width;
+    const y = (event.clientY - rect.top) / rect.height;
+    const tiltX = (x - 0.5) * this.settings.max;
+    const tiltY = (y - 0.5) * this.settings.max;
+
+    this.element.style.transform = `perspective(${this.settings.perspective}px) rotateX(${tiltY}deg) rotateY(${tiltX}deg) scale(${this.settings.scale})`;
+  }
+
+  reset() {
+    this.element.style.transform = "";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const tiltElements = document.querySelectorAll("[data-tilt]");
+  tiltElements.forEach(function (element) {
+    new VanillaTilt(element);
+  });
+});
